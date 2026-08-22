c6fc0f23536f69fbd222ad9f7ab28fcf96dd3890
maltepedersen <malpe24@student.sdu.dk>
2026-08-22 12:43:12 +0200

    deploy fix ?

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/personal-website',
+    // Only use basePath when building in GitHub Actions
+    basePath: isGithubActions ? '/personal-website' : '',
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;
