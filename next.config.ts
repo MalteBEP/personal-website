c2c7f72389a7bda43cb75798cd328d529163a50a
maltepedersen <malpe24@student.sdu.dk>
2026-08-22 12:47:25 +0200

    deploy.yml npm

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
 const nextConfig: NextConfig = {
     output: 'export',
-    // Only use basePath when building in GitHub Actions
-    basePath: isGithubActions ? '/personal-website' : '',
+    basePath:'/personal-website',
     images: {
         unoptimized: true,
     },
